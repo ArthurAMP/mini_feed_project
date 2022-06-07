1b6192a7b475ed56c374eb8f15e6ecdc0c341963
allangarcia <allan.garcia.agcs@gmail.com>
2022-06-07 13:13:58 -0300

    Add basic app

---
+++ lib/views/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: PostList(),
+    );
+  }
+}
+
+class PostList extends StatefulWidget {
+  const PostList({Key? key}) : super(key: key);
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
