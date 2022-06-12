d32a4843b2cbcca996cf859db8d5c4e4429236f8
allangarcia <allan.garcia.agcs@gmail.com>
2022-06-12 00:19:51 -0300

    Implement HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mini_feed_project/models/posts.dart';
+import 'package:mini_feed_project/remote/minifeed_api.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,21 +8,102 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: PostList(),
+      child: PostPagedListView(),
     );
   }
 }
 
-class PostList extends StatefulWidget {
-  const PostList({Key? key}) : super(key: key);
+class PostPagedListView extends StatefulWidget {
+  const PostPagedListView({Key? key}) : super(key: key);
 
   @override
-  State<PostList> createState() => _PostListState();
+  State<PostPagedListView> createState() => _PostPagedListViewState();
 }
 
-class _PostListState extends State<PostList> {
+class _PostPagedListViewState extends State<PostPagedListView> {
+  final _controller = ScrollController();
+  final _posts = <PostModel>[];
+  var _hasNextPage = true;
+  var _currentPage = 1;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    _controller.addListener(_scrollListener);
+    super.initState();
+    _fetchPosts();
+  }
+
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_isLoading &&
+        _hasNextPage) {
+      _fetchPosts();
+    }
+  }
+
+  void _fetchPosts() async {
+    setState(() => _isLoading = true);
+    var page = await MiniFeedAPI.getPostListPage(_currentPage);
+    if (page != null) {
+      if (page.posts.isNotEmpty) {
+        setState(() => _posts.addAll(page.posts));
+      }
+      if (page.isLastPage()) {
+        _hasNextPage = false;
+      } else {
+        _currentPage++;
+      }
+    }
+    setState(() => _isLoading = false);
+  }
+
+  Widget _buildListView() {
+    var itemCount = _posts.length;
+    if (_hasNextPage) itemCount++;
+    return ListView.separated(
+        controller: _controller,
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        itemBuilder: (context, index) {
+          if (index < _posts.length) {
+            return PostListItem(post: _posts[index]);
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: itemCount);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (_posts.isNotEmpty) return _buildListView();
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class PostListItem extends StatelessWidget {
+  const PostListItem({Key? key, required this.post}) : super(key: key);
+  final PostModel post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+              leading: const CircleAvatar(),
+              title: Text(
+                "Author ${post.authorId}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )),
+          ListTile(
+            title: Text(post.text),
+          )
+        ],
+      ),
+    );
   }
 }
