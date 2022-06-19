11a0fcb39e0053ad14d82f72f58ddc7976e2e04c
allan <allan.garcia.agcs@gmail.com>
2022-06-19 02:43:11 -0300

    Add resizeToAvoidBottomInset: false to avoid covering authentication buttons

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('MiniFeed'),
       ),
