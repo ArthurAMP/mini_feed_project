c96317b7925741c6d428a6edc7813ddd9e8eaa61
Thiago Nepomuceno <thi.nepo@gmail.com>
2022-07-17 10:09:31 -0300

    fix: added const modifiers

--- lib/views/new_post_page.dart
+++ lib/views/new_post_page.dart
@@ -7,24 +7,25 @@ class NewPostPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           child: Column(
             children: [
               Padding(
-                  padding: EdgeInsets.only(left: 15, right: 15, top: 25),
+                  padding: const EdgeInsets.only(left: 15, right: 15, top: 25),
                   child: Column(
                     children: [
-                      Row(children: [
+                      Row(children: const [
                         Text("New Post",
                             style: TextStyle(
                                 fontSize: 32,
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold)),
                       ]),
-                      SizedBox(
+                      const SizedBox(
                         height: 15,
                       ),
-                      Text("To create a new post you need to login first.",
+                      const Text(
+                          "To create a new post you need to login first.",
                           style: TextStyle(
                               fontSize: 20,
                               color: Colors.white,
