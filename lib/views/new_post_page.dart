14a4127760ea906bae246fbdc868f95778f1cae3
Thiago Nepomuceno <thi.nepo@gmail.com>
2022-07-17 10:08:10 -0300

    feat: initial New Post page

--- lib/views/new_post_page.dart
+++ lib/views/new_post_page.dart
@@ -5,6 +5,34 @@ class NewPostPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("New Post Page"));
+    return SafeArea(
+      child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(left: 15, right: 15, top: 25),
+                  child: Column(
+                    children: [
+                      Row(children: [
+                        Text("New Post",
+                            style: TextStyle(
+                                fontSize: 32,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold)),
+                      ]),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text("To create a new post you need to login first.",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  )),
+            ],
+          )),
+    );
   }
 }
